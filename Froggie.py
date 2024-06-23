93142c93880870e821b65dbe139a0809ef30eae3
Gestringc <cgestrin@students.kennesaw.edu>
2024-06-23 03:16:54 -0500

    Added Frog to screen, Keyboard inputs, jump sound

---
+++ Froggie.py
@@ -0,0 +1,115 @@
+# This file creates the Froggie sprite, defines movements, defines animation, and defines collision
+#-----------------------------------------------------------------------------------------------------------------------
+## Importing of needed functions and variables.##
+import pygame
+import sys
+from os import walk
+import os ## Debug
+
+#-----------------------------------------------------------------------------------------------------------------------
+## Defining and building of the player class. ##
+print(f'Current working directory: {os.getcwd()}')
+print(f'Exists? {os.path.isdir("./Images/Froggie")}')
+class Froggie(pygame.sprite.Sprite):
+    def __init__ (self, position, groups):
+        self.pos = pygame.math.Vector2(position)
+        super().__init__(groups)
+        self.import_assets()
+        print(self.animations) ## Debug
+        self.frame_index = 0
+        self.move_dir = "Forward"
+        self.image = self.animations["Forward"][self.frame_index]
+        self.rect = self.image.get_rect(center = position)
+        self.direction = pygame.math.Vector2((0, 0))
+        self.speed = 50
+        self.collision_objects = 0 ##Collision_Group
+        self.hitbox = self.rect.inflate(0, -self.rect.height/2)
+        self.Jump_Sound = pygame.mixer.Sound("./Music/Jump2.mp3")
+        self.Jump_Sound.set_volume(0.2)
+
+#-----------------------------------------------------------------------------------------------------------------------
+## Need to define the collision with objects, to be finished ##
+#-----------------------------------------------------------------------------------------------------------------------
+## Images for Froggie import ##
+    def import_assets(self):
+        Main_Path = "./Images/Froggie"
+        print(os.path.isdir(Main_Path)) ## Debug
+        self.animations = {}
+        for (index, folder) in enumerate(walk(Main_Path)):
+            if (index == 0):
+                for subfolder in folder[1]:
+                    self.animations[subfolder] = []
+            else:
+                for file in folder[2]:
+                    subfolder = folder[0].split("\\")[::-1][0]
+                    Image = Main_Path + "/" + subfolder + "/" + file
+                    surf = pygame.image.load(Image).convert_alpha()
+                    surf = pygame.transform.scale(surf, (100, 100))
+                    self.animations[subfolder].append(surf)
+
+#-----------------------------------------------------------------------------------------------------------------------
+## The movement for Froggie is defined here ##
+
+    def move_player(self, deltaTime):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+            self.pos.x += self.direction.x * self.speed * deltaTime
+            self.rect.centerx = round(self.pos.x)
+            self.hitbox.centerx = self.rect.centerx
+
+            self.pos.y += self.direction.y * self.speed * deltaTime
+            self.rect.centery = round(self.pos.y)
+            self.hitbox.centery = self.rect.centery
+
+#-----------------------------------------------------------------------------------------------------------------------
+## Keyboard imputs for player are defined, WASD and Arrow keys both allowed ##
+## Allows for continuous Keyboard Input ##
+
+    def input(self):
+        Counter = 0
+        keyboard_keys = pygame.key.get_pressed()
+        if (keyboard_keys[pygame.K_LEFT] or keyboard_keys[pygame.K_a]):
+            self.direction.x = -1
+            self.move_dir = 'Left'
+            Counter += 1
+            self.Jump_Sound.play()
+        elif (keyboard_keys[pygame.K_RIGHT ] or keyboard_keys[pygame.K_d]):
+            self.direction.x = 1
+            self.move_dir = 'Right'
+            Counter += 1
+            self.Jump_Sound.play()
+        else:
+            self.direction.x = 0
+
+        if (keyboard_keys[pygame.K_UP] or keyboard_keys[pygame.K_w]):
+            self.direction.y = -1
+            self.move_dir = 'Forward'
+            Counter += 1
+            self.Jump_Sound.play()
+        elif (keyboard_keys[pygame.K_DOWN] or keyboard_keys[pygame.K_s]):
+            self.direction.y = 1
+            self.move_dir = 'Down'
+            Counter += 1
+            self.Jump_Sound.play()
+        else:
+            self.direction.y = 0
+
+#-----------------------------------------------------------------------------------------------------------------------
+##
+
+    def animate_player(self, deltaTime):
+        if (self.direction.magnitude() != 0):
+            self.frame_index += 10 * deltaTime
+            if (self.frame_index >= len(self.animations[self.move_dir])):
+                self.frame_index = 0
+
+        else:
+            self.frame_index = 0
+
+        self.image = self.animations[self.move_dir][int(self.frame_index)]
+
+    def update(self, deltaTime):
+        self.input()
+        self.move_player(deltaTime)
+        self.animate_player(deltaTime)
--- Scripts/main.py
+++ Scripts/main.py
@@ -2,14 +2,18 @@
 import pygame
 import sys
 import random
-# from Froggie import Froggie --- Work in Progress
+from Froggie import Froggie
 
-#--------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 ## Initialization of pygame
 
 pygame.init()
 
-#-------------------------------------------------------------------------------------------------------------------------------
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+#-----------------------------------------------------------------------------------------------------------------------
 ## Game Window and Related Variables
 
 Game_Screen = pygame.display.set_mode((1500, 800))
@@ -17,21 +21,34 @@ Game_Screen.fill(color= (0, 255, 255) )
 pygame.display.flip()
 victory = False
 
-#----------------------------------------------------------------------------------------------------------------------------
-## Froggie Start Position is Defined Here ##
-Starting_Point = (750, 800) # Bottom of the y-axis, middle of the x-axis
-#Froggie = #XXX Need to build sprite and keyboard inputs
+#-----------------------------------------------------------------------------------------------------------------------
+## Froggie Sprite Generation  ##
+all_sprites = AllSprites()
+obstacle_sprites = pygame.sprite.Group()
+Starting_Point = (750, 800)# Bottom of the y-axis, middle of the x-axis
+Froggie = Froggie(Starting_Point, all_sprites)
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 ## The back ground music is imported and played in a loop ##
 
 Froggie_Music = pygame.mixer.Sound("./Music/Froggie_Music.mp3")
+Froggie_Music.set_volume(1)
 Froggie_Music.play(-1)
 
-#-----------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 ## Primary Game Loop ##
+# Create clock to get delta time later.
+Clock = pygame.time.Clock()
 while (True):
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             pygame.quit()
             sys.exit()
+
+    Delta_Time = Clock.tick(120)/1000
+# handle all sprites' updates
+    all_sprites.update(Delta_Time)
+
+    Game_Screen.fill((0, 255, 255))
+    all_sprites.draw(Game_Screen)
+    pygame.display.flip()
