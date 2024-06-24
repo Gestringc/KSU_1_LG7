f2cd050436ccaa147e8a76a736a5494a8e240a75
Gestringc <cgestrin@students.kennesaw.edu>
2024-06-24 16:00:55 -0500

    testing

--- Scripts/Cars.py
+++ Scripts/Cars.py
@@ -1,47 +1,28 @@
 # This file creates the Car sprite, defines movements, defines animation, and defines collision
 #-----------------------------------------------------------------------------------------------------------------------
-
+## Importing of needed functions and variables.##
+import pygame
+import sys
 
 #-----------------------------------------------------------------------------------------------------------------------
 ## Defining and building of the cars class. ##
 
+# Car Class
 class Car(pygame.sprite.Sprite):
+    def __init__(self, image, Position,*groups):
+        super().__init__(*groups)
+        self.image = image
+        self.rect = self.image.get_rect(bottomleft=(0, Position))
+        self.speed = 25
 
-    def __init__ (self, position, groups):
-        super().__init__(groups)
-        self.pos = pygame.math.Vector2(position)
-        self.import_assets()
-        print(self.animations)  ## Debug
-        self.frame_index = 0
-        self.move_dir = "Left"
-        self.image = self.animations["Left"][self.frame_index]
-        self.rect = self.image.get_rect(center=position)
-        self.direction = pygame.math.Vector2((0, 0)) ### what vector do cars need to be?
-        self.speed = 50  # what speed to cars need to be?
-        self.collision_objects = 0  ##Collision_Group
-        self.hitbox = self.rect.inflate(0, -self.rect.height / 2)
+    def update(self, dt):
+        self.rect.x += self.speed * dt
+        if self.rect.left > 1500:
+            self.kill()  # Death to all cars who travel from the game !!!!!!!!!!
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 ## Need to define the collision with objects, to be finished ##
-#-----------------------------------------------------------------------------------------------------------------------
-## Images for Cars import ##
-        redCar = pg.image.load("red.png").convert()
-        ##scale image?
-        screen.blit(redCar,) #what x,y are we putting car - lane 1?
-
-        GreenCar = pg.image.load("green.png").convert()
-        ##scale image?
-        screen.blit(GreenCar,) #what x,y are we putting car - lane 2?
-
-        YellowCar = pg.image.load("yellow.png").convert()
-        ##scale image?
-        screen.blit(YellowCar, )  # what x,y are we putting  car - lane 3?
-
- # -----------------------------------------------------------------------------------------------------------------------
-        ## The movement for cars is defined here ##
-
-
 
 
 
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,8 +1,8 @@
 ## Importation of needed modules, to be updated as needed ##
 import pygame
 import sys
-import random
-from Froggie import Froggie
+from Scripts.Froggie import Froggie
+from Cars import Car
 
 #-----------------------------------------------------------------------------------------------------------------------
 ## Initialization of pygame
@@ -21,7 +21,23 @@ Game_Screen.fill(color= (0, 255, 255))
 pygame.display.set_caption("Froggie")  #can change title if we decide
 pygame.display.flip()
 victory = False
+#-----------------------------------------------------------------------------------------------------------------------
+##### Here is where we will CREATE the car images with your code  ## *********************************************************************
+redCar = pygame.image.load("red.png").convert()
+redCar = pygame.transform.scale(redCar, (100, 100)) ## Adjust sizes as needed
 
+greenCar = pygame.image.load("green.png").convert()
+greenCar = pygame.transform.scale(greenCar, (100, 100)) ## Adjust sizes as needed
+
+yellowCar = pygame.image.load("yellow.png").convert()
+yellowCar = pygame.transform.scale(yellowCar, (100, 100)) ## Adjust Sizes as needed
+
+#************************************************************************************************************
+Clock = pygame.time.Clock()
+### create a list for car colors car_color = [] same as variable names above
+### create a list for time counters counters = [0, 0, 0] all zero to start
+### Create the three time intervals the cars will generate intervals = [x, y, z]
+roads = [800//3, 800//2, 2 * (800 // 3)]  # these will define the three different roads
 #-----------------------------------------------------------------------------------------------------------------------
 ## Froggie Sprite Generation  ##
 all_sprites = AllSprites()
@@ -51,5 +67,16 @@ while (True):
     all_sprites.update(Delta_Time)
 
     Game_Screen.fill((0, 255, 255))
+#************************ Here we will create a loop for actually generating the cars based off
+    # of time  ********************************************************
+    ## for i in range (3): 3 being the number of roads
+        # counter[i] += delta time
+            # if counter[i] > intervals[i]: #### Generate car
+                # counter[i] = 0
+                    #new_car = Car(cars_colors[i], (0, road_y_positions[i]))
+                    #cars_colors[i], cars_colors[i - 1] = cars_colors[i - 1], cars_colors[i]  # Cycle car color
+                    #all_sprites.add(new_car)
+
+    ###obstacle_sprites.add(new_car)  ADD CAR VARIABLE HERE
     all_sprites.draw(Game_Screen)
     pygame.display.flip()
