15437c478fc9da8b80577319f3f6d8d0f27fc8e4
bncle <bcleme18@kennesaw,edu>
2024-07-07 16:06:00 -0400

    testing

--- Scripts/main.py
+++ Scripts/main.py
@@ -19,7 +19,7 @@ class AllSprites(pygame.sprite.Group):
 ## Game Window and Related Variables
 
 Game_Screen = pygame.display.set_mode((1500, 800))
-grass_image = pygame.image.load("./Images/Backgrounds/Grass1.jpg")
+# grass_image = pygame.image.load("./Images/Backgrounds/Grass1.jpg")
 Game_Screen.fill(color= (0, 255, 255))
 pygame.display.set_caption("Froggie")  #can change title if we decide
 pygame.display.flip()
@@ -27,20 +27,37 @@ victory = False
 font = pygame.freetype.Font(None, 36)
 #-----------------------------------------------------------------------------------------------------------------------
 ##### Here is where we will CREATE the car images with your code  ## *********************************************************************
-##redCar = pygame.image.load("red.png").convert()
-##redCar = pygame.transform.scale(redCar, (100, 100)) ## Adjust sizes as needed
+redCar = pygame.image.load("../Cars/red.png").convert_alpha()
+redCar_scale = pygame.transform.scale(redCar, (50, 50)) ## Adjust sizes as needed
+redCar_scale_rect = pygame.Rect(0, 800//3, 50, 50)
+# redCar_scale_rect.x = 0
+# redCar_scale_rect.y = 800//3
+# speed1 = [2]
 
-##greenCar = pygame.image.load("green.png").convert()
-##greenCar = pygame.transform.scale(greenCar, (100, 100)) ## Adjust sizes as needed
+greenCar = pygame.image.load("../Cars/green.png").convert_alpha()
+greenCar_scale = pygame.transform.scale(greenCar, (50, 50))## Adjust sizes as needed
+greenCar_scale_rect = pygame.Rect(0, 800//2, 50, 50)
+# greenCar_scale_rect.x = 0
+# greenCar_scale_rect.y = 800//2
+# speed2 = [2]
 
-##yellowCar = pygame.image.load("yellow.png").convert()
-##yellowCar = pygame.transform.scale(yellowCar, (100, 100)) ## Adjust Sizes as needed
+yellowCar = pygame.image.load("../Cars/yellow.png").convert_alpha()
+yellowCar_scale = pygame.transform.scale(yellowCar, (50, 50)) ## Adjust Sizes as needed
+yellowCar_scale_rect = pygame.Rect(0, 2 * (800 // 3), 50, 50)
+# yellowCar_scale_rect.x = 0
+# yellowCar_scale_rect.y = 2 * (800 // 3)
+# speed3 = [2]
+
+
 
 #************************************************************************************************************
 Clock = pygame.time.Clock()
-### create a list for car colors car_color = [] same as variable names above
-### create a list for time counters counters = [0, 0, 0] all zero to start
+### create a list for car colors same as variable names above
+car_colors = [redCar_scale, greenCar_scale, yellowCar_scale]
+# car_speeds = [speed1, speed2, speed3]
+counter = [0 ,0 ,0]
 ### Create the three time intervals the cars will generate intervals = [x, y, z]
+intervals = [1, 3, 0.5]
 roads = [800//3, 800//2, 2 * (800 // 3)]  # these will define the three different roads
 #-----------------------------------------------------------------------------------------------------------------------
 ## Froggie Sprite Generation  ##
@@ -51,11 +68,11 @@ Froggie = Froggie(Starting_Point, all_sprites)
 
 #-----------------------------------------------------------------------------------------------------------------------
 ## The back ground music is imported and played in a loop ##
-
-Froggie_Music = pygame.mixer.Sound("./Music/Music1.mp3")
-Froggie_Music.set_volume(.2)
-Froggie_Music.play(-1)
-#-----------------------------------------------------------------------------------------------------------------------
+#
+# Froggie_Music = pygame.mixer.Sound("./Music/Music1.mp3")
+# Froggie_Music.set_volume(.2)
+# Froggie_Music.play(-1)
+# #-----------------------------------------------------------------------------------------------------------------------
 ## Primary Game Loop ##
 # Create clock to get delta time later.
 Clock = pygame.time.Clock()
@@ -68,10 +85,10 @@ while (True):
     Delta_Time = Clock.tick(120)/1000
 # handle all sprites' updates
     all_sprites.update(Delta_Time)
-
-    for i in range(0, 1500, grass_image.get_width()):
-        for j in range(0, 800, grass_image.get_height()):
-            Game_Screen.blit(grass_image, (i, j))
+    #
+    # for i in range(0, 1500, grass_image.get_width()):
+    #     for j in range(0, 800, grass_image.get_height()):
+    #         Game_Screen.blit(grass_image, (i, j))
 
     if Froggie.rect.y <= 0:
         text_surface, rect = font.render("Froggie Lives!!!", (255,255,255), size=50)
@@ -79,16 +96,20 @@ while (True):
         all_sprites.draw(Game_Screen)
         pygame.display.flip()
 
-    ##Game_Screen.fill((0, 255, 255))
+    Game_Screen.fill((0, 255, 255))
+
+    Game_Screen.blit(redCar_scale, (0, (800 // 3)))
+    Game_Screen.blit(greenCar_scale, (0, (800 // 2)))
+    Game_Screen.blit(yellowCar_scale, (0, 2 * (800 // 3)))
 #************************ Here we will create a loop for actually generating the cars based off
     # of time  ********************************************************
-    ## for i in range (3): 3 being the number of roads
-        # counter[i] += delta time
-            # if counter[i] > intervals[i]: #### Generate car
-                # counter[i] = 0
-                    #new_car = Car(cars_colors[i], (0, road_y_positions[i]))
-                    #cars_colors[i], cars_colors[i - 1] = cars_colors[i - 1], cars_colors[i]  # Cycle car color
-                    #all_sprites.add(new_car)
+    for i in range (3): # 3 being the number of roads
+        counter[i] += Delta_Time
+        if counter[i] > intervals[i]: #### Generate car
+                counter[i] = 0
+                new_car = (car_colors[i], (0, roads[i]))
+                car_colors[i], car_colors[i - 1] = car_colors[i - 1], car_colors[i]  # Cycle car color
+                # all_sprites.add(new_car)
 
     ###obstacle_sprites.add(new_car)  ADD CAR VARIABLE HERE
     all_sprites.draw(Game_Screen)
