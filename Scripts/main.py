3e14cf4b52665f54f1bb4eed4e5d101883bb53df
Gestringc <cgestrin@students.kennesaw.edu>
2024-07-07 23:12:37 -0500

    Changes

--- Scripts/main.py
+++ Scripts/main.py
@@ -20,7 +20,36 @@ class Car:
         self.image = img
         self.speed = speed
         self.rect = rect
+#-----------------------------------------------------------------------------------------------------------------------
+## Game Over Screen Function ##
+def end_game_screen(message):
+    running = True
+    retry_text, retry_button = button_font.render("Play Again", (255, 255, 255), size=36)
+    quit_text, quit_button = button_font.render("Quit", (255, 255, 255), size=36)
 
+    retry_rect = pygame.Rect(Game_Screen.get_width() / 2 - retry_button.width / 2, Game_Screen.get_height() / 2,
+                             retry_button.width, retry_button.height)
+    quit_rect = pygame.Rect(Game_Screen.get_width() / 2 - quit_button.width / 2, Game_Screen.get_height() * 2 / 3,
+                            quit_button.width, quit_button.height)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if retry_rect.collidepoint(event.pos):
+                    return True
+                elif quit_rect.collidepoint(event.pos):
+                    return False
+
+        Game_Screen.blit(dead_image, (350, 200))
+        text_surface, rect = font.render(message, (255, 255, 255), size=100)
+        Game_Screen.blit(text_surface, (Game_Screen.get_width() / 2 - rect.width / 2, 45))
+        Game_Screen.blit(retry_text, retry_rect)
+        Game_Screen.blit(quit_text, quit_rect)
+        pygame.display.flip()
+
+    return False
 #-----------------------------------------------------------------------------------------------------------------------
 ## Game Window and Related Variables
 
@@ -31,6 +60,9 @@ pygame.display.set_caption("Froggie")  #can change title if we decide
 pygame.display.flip()
 victory = False
 font = pygame.freetype.Font(None, 36)
+dead_image = pygame.image.load("./Images/Froggie/zzz/Frog_Died.png").convert_alpha()
+dead_image = pygame.transform.scale(dead_image, (800, 400))
+button_font = pygame.freetype.Font(None, 36)
 #-----------------------------------------------------------------------------------------------------------------------
 ##### Here is where we will CREATE the car images with your code  ## *********************************************************************
 redCar = pygame.image.load("./Cars/red.png").convert_alpha()
@@ -60,10 +92,11 @@ car_positions = [pygame.Rect(0, 150, 50, 50), pygame.Rect(0, 400, 50, 50),
                  pygame.Rect(0, 650, 50, 50)]
 cars = [Car(img, speed, pos) for img, speed, pos in zip(car_images, car_speeds, car_positions)]
 
+car_color_index = 0
 #**************************************************************************************************************
 #decor images
 
-road = pygame.image.load("./Images/Objects/road.png").convert_alpha()
+road = pygame.image.load("./Images/Backgrounds/road.jfif").convert_alpha()
 road_scale = pygame.transform.scale(road,(2000,80))
 
 
@@ -97,6 +130,7 @@ while (True):
         if (event.type == pygame.QUIT):
             pygame.quit()
             sys.exit()
+
     Game_Screen.fill(color=(0, 255, 255))
     Delta_Time = Clock.tick(60)/1000
     #ticker = Clock.tick(10)
@@ -112,10 +146,6 @@ while (True):
     Game_Screen.blit(road_scale, (-50, 400))
     Game_Screen.blit(road_scale, (-50, 650))
 
-    if Froggie.rect.y <= 0:
-        text_surface, rect = font.render("Froggie Lives!!!", (255,255,255), size=50)
-        Game_Screen.blit(text_surface,(Game_Screen.get_width() / 2 - rect.width / 2, Game_Screen.get_height() / 2 - rect.height / 2))
-        all_sprites.draw(Game_Screen)
     cars_to_remove = []
     for car in cars:
         car.rect.x += car.speed * Delta_Time  # Update the car's position.
@@ -123,21 +153,59 @@ while (True):
             cars_to_remove.append(car)
         else:
             Game_Screen.blit(car.image, car.rect)
+            frog_mask = pygame.mask.from_surface(Froggie.image)
+            car_mask = pygame.mask.from_surface(car.image)
+            offset = (Froggie.rect.topleft[0] - car.rect.topleft[0],
+                      Froggie.rect.topleft[1] - car.rect.topleft[1])
+            collision_point = frog_mask.overlap(car_mask, offset)
+            if pygame.sprite.collide_mask(Froggie, car):
+                result = end_game_screen("You killed Froggie!!!")
+                Froggie.pos = pygame.math.Vector2(Starting_Point)  # update the Vector2 position
+                Froggie.rect.topleft = Starting_Point  # update the topleft rect attribute
+                Froggie.image = dead_image
+                pygame.mixer.pause()  # pause game music
+                pygame.display.flip
+                if result:
+                    # reset game
+                    Froggie.image = pygame.image.load("./Images/Froggie/Down/Forward.png")  # reset froggie sprite
+                    Froggie.rect.topleft = Starting_Point  # reposition froggie to the start
+                    cars = []  # remove all cars
+                    counter = [0, 0, 0]  # reset car generation timer
+                    pygame.mixer.unpause()  # resume the music
+                else:
+                    pygame.quit()
+                    sys.exit()
+            elif  Froggie.rect.y <= 0:
+                result = end_game_screen("Froggie Lives!!!")
+                Froggie.pos = pygame.math.Vector2(Starting_Point)  # update the Vector2 position
+                Froggie.rect.topleft = Starting_Point  # update the topleft rect attribute
+                Froggie.image = dead_image
+                pygame.mixer.pause()  # pause game music
+                pygame.display.flip
+                if result:
+                # reset game
+                    Froggie.image = pygame.image.load("./Images/Froggie/Down/Forward.png")  # reset froggie sprite
+                    Froggie.rect.topleft = Starting_Point  # reposition froggie to the start
+                    cars = []  # remove all cars
+                    counter = [0, 0, 0]  # reset car generation timer
+                    pygame.mixer.unpause()  # resume the music
+                else:
+                    pygame.quit()
+                    sys.exit()
 
     for car in cars_to_remove:
         cars.remove(car)
 
     # Car generation
+    color_indexes = [0, 1, 2]
     for i in range(3):
         counter[i] += Delta_Time
         if counter[i] >= intervals[i]:
             counter[i] = 0
-            car_image = pygame.transform.scale(car_images[i], (50, 50))  # scale car image
-            new_car = Car(car_image, car_speeds[i], pygame.Rect(0, roads[i], 50,
-                                                                50))  # create new car using Car class initializer and speed values from "car_speeds"
+            car_image = pygame.transform.scale(car_images[car_color_index % len(car_images)], (50, 50))
+            new_car = Car(car_image, car_speeds[i], pygame.Rect(0, roads[i], 50, 50))  # create new car
             cars.append(new_car)
-
-    ###obstacle_sprites.add(new_car)  ADD CAR VARIABLE HERE
+            car_color_index += 1
 
     all_sprites.draw(Game_Screen)
     pygame.display.flip()
